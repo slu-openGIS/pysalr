ec559836281e1679ac6860cb4d13ba911c29e32d
Christopher Prener <chris.prener@slu.edu>
2019-03-26 11:18:59 -0500

    update scripts based on R CMD check

--- R/pyr_install.R
+++ R/pyr_install.R
@@ -2,10 +2,12 @@
 #'
 #' @description Add the \code{saveModelOutput.py} to a project.
 #'
-#' @param path
+#' @param path File path where script should be placed
+#' @param overwrite Overwrite files if they exist. If this is FALSE and the file exists
+#'     an error will be thrown.
 #'
 #' @export
-pyr_install <- function(path){
+pyr_install <- function(path, overwrite = TRUE){
 
   # copy file
   fs::file_copy(system.file("extdata", "saveModelOutput.py", package = "pysalr", mustWork = TRUE),
